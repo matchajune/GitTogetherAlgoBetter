30ad8928b97e6f2bcbd82d9ee1d651d26504657f
Julius Jung <me@matchajune.io>
2023-05-25 11:13:16 -0700

    add note to two_sum.js to not solve yet

--- challenges/todo/two_sum.js
+++ challenges/todo/two_sum.js
@@ -1,4 +1,6 @@
 /*
+NOTE: Don't solve this yet! Just posting so you can start thinking about the problem!
+
 Given an array of integers `nums` and an integer `target`, return indices of the two numbers such that they add up to `target`.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 You can return the answer in any order.
